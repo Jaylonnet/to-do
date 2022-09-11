503d3880f084adeca46c160152585b80f9e78129
Jaylon <jaylonwingfield10@gmail.com>
2022-09-11 22:17:59 +0100

    Imported objects and createProject function created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import './style.css';
 import {counter} from './counter';
+import {displayProject} from './dom';
+import {ToDoItem, Project} from './todo'
 
 const taskCounter = counter();
 const projectCounter = counter();
 
+const projects = [];
+let selectedProject = null;
+
+const createProjectForm = document.querySelector('#new-project-form');
+const createProjectBtn = document.querySelector('#new-project-btn');
+createProjectBtn.addEventListener('click', (e) => {
+    let title = createProjectForm.elements['project-title'];
+    createProject(title.value);
+    title.value = ''
+});
+
+function createProject(title) {
+    let projectId = projectCounter.getUniqueId();
+    const project = Project(projectId, title);
+    projects.push(project);
+    displayProject(project);
+    document.querySelector(`#project-list li[data-projectid="${projectId}"]`).addEventListener('click', () => {
+        selectedProject = project;
+    });
+};
