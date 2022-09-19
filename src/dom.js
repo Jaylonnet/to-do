5175a90a1f23fa6f71bfaacaea3d36192b21e558
Jaylon <jaylonwingfield10@gmail.com>
2022-09-19 17:25:10 +0100

    Renamed removeProjectFromDOM function to removeFromDOM

--- src/dom.js
+++ src/dom.js
@@ -17,8 +17,12 @@ export function displayProject(project) {
     projectList.append(li);
 };
 
-export function removeProjectFromDOM(projectId) {
-    document.querySelector(`li[data-project-id="${projectId}"]`).remove();
+export function removeFromDOM(id, type) {
+    if (type == 'project') {
+        document.querySelector(`li[data-project-id="${id}"]`).remove();
+    } else if (type == 'task' ) {
+        document.querySelector(`li[data-task-id="${id}"]`).remove();
+    }
 };
 
 export function displayTask(task) {
